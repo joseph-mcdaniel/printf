d0554c2602a376bc0f887939d3f85e53a9a1e3b5
jarehec <bone1198@gmail.com>
2017-03-20 02:24:17 +0000

    added _convert prototype

--- holberton.h
+++ holberton.h
@@ -1,9 +1,9 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-#include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 int _printf(const char *format, ...);
-void _convert(char a, va_list args);
 int _putchar(char c);
+int _convert(char a, va_list args, unsigned int chars);
+int _puts(char *str, unsigned int chars);
 #endif
