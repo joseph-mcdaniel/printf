17630a8a73e0e607e29cea0f45adbfedb76b8cef
jarehec <bone1198@gmail.com>
2017-03-20 21:58:27 +0000

    tests for 0 & 1

---
+++ test_files/test_all.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+int main(void)
+{
+	_printf("Char: %c\n", 'c');
+	_printf("String: %s\n", "hello world");
+	_printf("Percent: %%\n");
+	_printf("Decimal: %d\n", 12.3);
+	_printf("Integer: %i\n", 34);
+
+	return (0);
+}
