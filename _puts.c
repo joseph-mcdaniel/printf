133ed72f694708ec7727df1a8d2e590fc1073726
jarehec <bone1198@gmail.com>
2017-03-20 02:27:18 +0000

    _puts implematation that returns char count

---
+++ _puts.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* _puts - prints a string
+* @str: string input
+* @chars: character count
+*
+* Return: number of characters
+*/
+int _puts(char *str, unsigned int chars)
+{
+	unsigned int index;
+
+	for (index = 0; str[index] != '\0'; index++)
+	{
+		chars++;
+		_putchar(str[index]);
+	}
+	return (chars);
+}
