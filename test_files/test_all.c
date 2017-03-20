2d61a75301619aec6e921cd096f1b31603e907ca
jarehec <bone1198@gmail.com>
2017-03-20 22:57:17 +0000

    added test file

--- test_files/test_all.c
+++ test_files/test_all.c
@@ -1,12 +1,21 @@
 #include "holberton.h"
-
+#include <stdio.h>
 int main(void)
 {
-	_printf("Char: %c\n", 'c');
-	_printf("String: %s\n", "hello world");
-	_printf("Percent: %%\n");
-	_printf("Decimal: %d\n", 12.3);
-	_printf("Integer: %i\n", 34);
+	int a, b;
 
+	a = _printf("Char: %c\n", 'c');
+	a += _printf("String: %s\n", "hello world");
+	a += _printf("Percent: %%\n");
+	a += _printf("Decimal: %d\n", 12);
+	a +=_printf("Integer: %i\n", 34);
+	_printf("Total: %d\n", a);
+
+	b = printf("Char: %c\n", 'c');
+	b += printf("String: %s\n", "hello world");
+	b += printf("Percent: %%\n");
+	b += printf("Decimal: %d\n", 12);
+	b += printf("Integer: %i\n", 34);
+	printf("Total: %d\n", b);
 	return (0);
 }
