78da84b634a30a7a0b9b84f2fa436231c9980678
jarehec <bone1198@gmail.com>
2017-03-21 22:25:31 +0000

    check for null

--- _printf.c
+++ _printf.c
@@ -11,6 +11,9 @@ int _printf(const char *format, ...)
 	int i;
 	unsigned int chars = 0;
 
+	if (format == NULL)
+		return (-1);
+
 	va_start(arguments, format);
 	for (i = 0; format[i] != '\0'; i++)
 	{
