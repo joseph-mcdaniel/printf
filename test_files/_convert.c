ae009cbf0b7a645442a7a9dc697260e584030ffa
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2017-03-21 01:28:51 +0000

    Remove unnecessary lines

--- test_files/_convert.c
+++ test_files/_convert.c
@@ -20,19 +20,18 @@ unsigned int _convert(char a, va_list args, unsigned int chars)
 			tmp = va_arg(args, char *);
 			if (tmp == NULL)
 				return (0);
-			chars = _puts(tmp, chars);
+			chars += _puts(tmp, chars);
 			break;
-	case 'i':
-		print_number(va_arg(args, int));
-		chars++;
-		break;
-	case 'd':
-		print_number(va_arg(args, int));
-		chars++;
-		break;
-		default:
-			_putchar(a);
+	        case 'i':
+			print_number(va_arg(args, int));
 			chars++;
+			break;
+	        case 'd':
+	        	print_number(va_arg(args, int));
+			chars++;
+			break;
+	        default:
+			chars += _putchar(a);
 			break;
 	}
 	return (chars);
